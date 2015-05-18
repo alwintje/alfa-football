122d81a4e613e29bd99cdb522f00937d095247d9
Alwin <alwinkroesen@gmail.com>
2015-05-18 13:43:00 +0200

    html

--- includes/Header.php
+++ includes/Header.php
@@ -11,6 +11,14 @@
 
 <div id="header">
     <div class="inner">
-        <img src="img/logo.png" />
+        <div class="logo">
+            <img src="img/logo.png" />
+        </div>
+        <div class="navigation">
+            <ul>
+                <li>item 1</li>
+                <li>item 2</li>
+            </ul>
+        </div>
     </div>
 </div>
