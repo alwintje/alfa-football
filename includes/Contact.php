2937d9f7599a28ea2c025b874c270e08a18be6a3
yaron <y.lambers@outlook.com>
2015-05-18 19:06:30 +0200

    Contact toegevoegd

---
+++ includes/Contact.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yaron
+ * Date: 18-5-2015
+ * Time: 18:47
+ */
+?>
+<div id="contact" class="scroll-url container">
+    <div class="head">
+        <h1>Contact</h1>
+    </div>
+    <div class="content">
+        <div class="form">
+            <form action="#">
+                <label>Naam:</label><input type="text" class="form-control"/>
+                <label>Naam:</label><input type="text" class="form-control"/>
+                <label>Naam:</label><input type="text" class="form-control"/>
+                <label>Naam:</label><input type="text" class="form-control"/>
+                <button class="btn btn-default">Verzend</button>
+            </form>
+
+        </div>
+
+    </div>
+</div>
--- index.php
+++ index.php
@@ -15,14 +15,15 @@
     <meta name="viewport" content="initial-scale=1; maximum-scale=1; user-scalable=false;width=device-width, height=device-height" >
 
     <title>Alfa-football</title>
-
     <link href="css/index.css" rel="stylesheet" />
     <link href="css/header.css" rel="stylesheet" />
     <link href="css/Games.css" rel="stylesheet" />
+    <link href="css/Contact.css" rel="stylesheet" />
     <link rel="stylesheet" type="text/css" href="css/component.css" />
     <link rel="stylesheet" href="css/transitions.php?duration=1000&transition=easeInCirc">
     <link rel="stylesheet" href="css/slider.css">
     <link rel="stylesheet" type="text/css" href="css/custom.css" />
+
 
     <script src="js/modernizr.custom.js"></script>
     <script src="js/menu.js"></script>
@@ -40,8 +41,6 @@
         var transition = 'easeInCirc';// easeInCirc easeInQuad
     </script>
     <script src="js/slider.js" ></script>
-
-
 </head>
 <body>
     <?php
@@ -49,6 +48,7 @@
         require_once($includeFolder."/Header.php");
         require_once($includeFolder."/Content.php");
         require_once($includeFolder."/Games.php");
+        require_once($includeFolder."/Contact.php");
     ?>
     <div style="height: 3000px;"></div>
     <?php
