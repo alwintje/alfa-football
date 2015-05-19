000e972dba84952706113836dac4c33cf9bb8aef
Ylambers <y.lambers@outlook.com>
2015-05-19 09:04:05 +0200

    Test

---
+++ database.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yaron
+ * Date: 19-5-2015
+ * Time: 08:42
+ */
+
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+
+mysql_connect($host, $user, $pass);
+$dbName = "";
+mysql_select_db($dbName);
+
