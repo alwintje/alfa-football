6b1b94f879d5758f68748feac19fc44b88554022
Ylambers <y.lambers@outlook.com>
2015-05-19 11:01:33 +0200

    Test

--- includes/Footer.php
+++ includes/Footer.php
@@ -6,13 +6,14 @@
  * Time: 18:47
  */
 ?>
+
 <div id="contact" class="scroll-url container">
     <div class="head">
         <h1>Contact</h1>
     </div>
     <div class="content">
         <div class="form">
-            <form action="#">
+            <form method="post" action="includes/PostContactForm.php">
                 <label for="name">Naam</label><input name="name" id="name" type="text" class="form-control"/>
                 <label for="email">Email</label><input name="email" id="email" type="text" class="form-control"/>
                 <label for="message">bericht</label><textarea name="message" id="message" class="form-control"></textarea>
---
+++ includes/PostContactForm.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yaron
+ * Date: 19-5-2015
+ * Time: 09:59
+ */
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+
+mysql_connect($host, $user, $pass);
+
+$dbName = "school";
+
+$con = mysql_connect("localhost", "root", "");
+mysql_select_db($dbName);
+
+if(mysql_error()){
+    echo "connection to the database failed!";
+}
+
+if(mysql_query("INSERT INTO football_contact(name, email, message)VALUES(
+        '".mysql_real_escape_string($_POST['name'])."',
+        '".mysql_real_escape_string($_POST['email'])."',
+        '".mysql_real_escape_string($_POST['message'])."'
+    )"))
+{
+    echo "verzonden";
+}else{
+    echo "Er is helaas een fout op getreden";
+}
+
+mysql_close($con);
