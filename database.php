1d8864a81c25c52e0aaa31c1a295c6134d62391f
Alwin <alwinkroesen@gmail.com>
2015-05-19 09:06:13 +0200

    een boel

--- database.php
+++ database.php
@@ -1,17 +1,39 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: yaron
- * Date: 19-5-2015
- * Time: 08:42
+ * User: Alwin
+ * Date: 3-3-2015
+ * Time: 15:50
  */
 
+class Database{
 
-$host = "localhost";
-$user = "root";
-$pass = "";
+    private $dbsettings = Array(
+        //Dit moet aangepast worden.
 
-mysql_connect($host, $user, $pass);
-$dbName = "";
-mysql_select_db($dbName);
+        "server"        => "localhost",	// MySQL server naam.
+        "user"          => "root",		// MySQL gebruikersnaam.
+        "pass"          => "",			// MySQL wachtwoord.
+        "name"          => "proict",	// MySQL database naam.
+        "prefix"        => "chat");		// Eerste deel van tabellen.
 
+    private $mysqli;
+    public function opendb() { // Maakt connectie met de database.
+
+        extract($this->dbsettings);
+        $this->mysqli = new mysqli($server, $user, $pass, $name);
+
+        if ($this->mysqli->connect_errno) {
+            echo "Failed to connect to MySQL: (" . $this->mysqli->connect_errno . ") " . $this->mysqli->connect_error;
+        }
+        //mysql_set_charset('utf8',$mysqli);
+        return $this->mysqli;
+
+    }
+
+    public function doquery($query, $table) { // Een kleine hulpmiddel.
+        $sqlquery = $this->mysqli->query(str_replace("{{table}}", $this->dbsettings["prefix"] . "_" . $table, $query));
+        return $sqlquery;
+
+    }
+}
