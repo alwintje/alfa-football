7f2a74bcd1e4b4c7015f3ba61966cb1b19cfcdd8
Alwin <alwinkroesen@gmail.com>
2015-05-20 16:45:02 +0200

    een boel

--- admin/includes/Ajax.php
+++ admin/includes/Ajax.php
@@ -6,8 +6,16 @@
  * Time: 15:12
  */
 require_once("../../includes/Database.php");
+$db = new Database();
+$db->opendb();
 if(isset($_GET['reviews'])){
-    echo $_POST['start'];
-    echo "<br />";
-    echo $_POST['end'];
+    $sql = $db->doquery("SELECT * FROM {{table}} LIMIT ".$_POST['start'].",".$_POST['end'], "reviews");
+    $list = [];
+    while($row = mysqli_fetch_array($sql)){
+        $i = count($list);
+        $list[$i] = $row;
+    }
+    echo json_encode($list);
+
 }
+?>
--- admin/js/script.js
+++ admin/js/script.js
@@ -18,7 +18,7 @@ function reviews(){
 
     $.ajax({
         type: "POST",
-        url: "admin/includes/ajax.php?reviews",
+        url: "admin/includes/Ajax.php?reviews",
         data: {start: rev_start,end: rev_end},
         dataType: 'json'
     }).done(function(data){
@@ -29,10 +29,25 @@ function reviews(){
         //    searchContent.style.display = "none";
         //}
         //createDivs(searchContent, searchString);
-        //for(var i =0;i < data.length;i++){
-        //    createDivs(searchContent, "<a href='?readMore="+data[i].id+"'>"+data[i].title+"</a> <br />");
-        //}
+        var table = document.createElement("table");
+        table.style.width = "100%";
+        for(var i =0;i < data.length;i++){
+            //createDivs(searchContent, "<a href='?readMore="+data[i].id+"'>"+data[i].title+"</a> <br />");
+            console.log(data[i].title);
+            var tr = document.createElement("tr");
+            var td = document.createElement("td");
+            td.innerHTML = data[i].title;
+            tr.appendChild(td);
+            td = document.createElement("td");
+            td.innerHTML = data[i].rev_date;
+            tr.appendChild(td);
+            table.appendChild(tr);
+        }
+        content.appendChild(table);
 
-        console.log(data);
+        //console.log(data);
     });
+
+
+
 }
