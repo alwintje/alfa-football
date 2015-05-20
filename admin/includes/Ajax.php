346cb48cc863bf6da96839f3f17b42e8253031aa
Alwin <alwinkroesen@gmail.com>
2015-05-20 15:40:40 +0200

    een boel

--- admin/includes/Ajax.php
+++ admin/includes/Ajax.php
@@ -5,7 +5,9 @@
  * Date: 20-5-2015
  * Time: 15:12
  */
-
+require_once("../../includes/Database.php");
 if(isset($_GET['reviews'])){
-
+    echo $_POST['start'];
+    echo "<br />";
+    echo $_POST['end'];
 }
--- admin/js/script.js
+++ admin/js/script.js
@@ -4,4 +4,35 @@
 
 $(document).ready(function(){
 
+
+    content.innerHTML = "moii";
+
+    reviews();
 });
+
+var rev_start = 0;
+var rev_end = 10;
+function reviews(){
+
+    var content = document.querySelector("#reviewContents");
+
+    $.ajax({
+        type: "POST",
+        url: "admin/includes/ajax.php?reviews",
+        data: {start: rev_start,end: rev_end},
+        dataType: 'json'
+    }).done(function(data){
+        //
+        //searchContent.style.display = "block";
+        //searchContent.innerHTML = "";
+        //if(data.length == 0){
+        //    searchContent.style.display = "none";
+        //}
+        //createDivs(searchContent, searchString);
+        //for(var i =0;i < data.length;i++){
+        //    createDivs(searchContent, "<a href='?readMore="+data[i].id+"'>"+data[i].title+"</a> <br />");
+        //}
+
+        console.log(data);
+    });
+}
