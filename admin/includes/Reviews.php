dc3344e91eaccd6fcc709bd89b594655b6ed1f67
Ylambers <y.lambers@outlook.com>
2015-05-20 15:09:19 +0200

    Test

--- admin/includes/Reviews.php
+++ admin/includes/Reviews.php
@@ -21,22 +21,46 @@
 <!--                 <label for="rev_date">date</label><br/><input name="rev_date" id="rev_date" class="form-control" required><br/>-->
 <!--                <label for="image">image</label><input name="image" id="message" class="form-control" required>-->
                 <?php
+                    $values = [
+                        "title"=>"",
+                        "intro"=>"",
+                        "content"=>"",
+                        "author"=>"",
+                        "date"=>"",
+                        "image"=>""
+                    ];
+
                     if(isset($_POST['addReview'])){
+
+
                         $title = "title='".mysql_real_escape_string($_POST['title'])."'";
                         $intro = "intro='".mysql_real_escape_string($_POST['intro'])."'";
                         $content = "content='".mysql_real_escape_string($_POST['content'])."'";
                         $author = "author='".mysql_real_escape_string($_POST['author'])."'";
                         $date = "rev_date='".mysql_real_escape_string($_POST['rev_date'])."'";
                         $image = "image='".mysql_real_escape_string($_POST['image'])."'";
+
                         $errors = [];
-                        if(count($title) < 4){$errors[] = "Titel is niet lang genoeg.";}
-                        if(count($title) < 4){$errors[] = "Titel is niet lang genoeg.";}
-                        if(count($title) < 4){$errors[] = "Titel is niet lang genoeg.";}
+
+                        if(strlen($_POST['title']) < 4){$errors[] = "Titel is niet lang genoeg.";}
+                        if(strlen($_POST['intro']) < 4){$errors[] = "Intro is niet lang genoeg.";}
+                        if(strlen($_POST['content']) < 4){$errors[] = " Inhoud is niet lang genoeg.";}
+                        if(strlen($_POST['author']) < 4){$errors[] = " Auteur is niet lang genoeg.";}
+                        if(strlen($_POST['rev_date']) < 4){$errors[] = " Datum is niet gekozen.";}
+                        //if(strlen($_POST['image']) < 4){$errors[] = " Acteur is niet lang genoeg.";}
 
                         if(count($errors) == 0){
                             $db->doquery("INSERT INTO {{table}} SET $title, $intro, $content ,$date,$author,$image","reviews");
                         }else{
 
+                            $values = [
+                                "title"=>$_POST['title'],
+                                "intro"=>$_POST['intro'],
+                                "content"=>$_POST['content'],
+                                "author"=>$_POST['author'],
+                                "date"=>$_POST['rev_date'],
+                                "image"=>$_POST['image'],
+                            ];
                             foreach($errors as $val){
                                 echo $val."<br />";
                             }
@@ -44,12 +68,12 @@
                     }
                 ?>
 
-                <label for="title">Titel</label><input name="title" id="title" type="text" class="form-control"/>
-                <label for="intro">Inleiding</label><input name="intro" id="intro" type="text" class="form-control"/>
-                <label for="content">Bericht</label><textarea name="content" id="content" class="form-control"></textarea>
-                <label for="author">Maker</label><input name="author" id="author" class="form-control" >
-                <label for="rev_date">Datum</label><br/><input name="rev_date" id="rev_date" class="form-control"><br/>
-                <label for="image">Afbeelding</label><input name="image" id="message" class="form-control" >
+                <label for="title">Titel</label><input name="title" id="title" value="<?php echo $values['title'];?>" type="text" class="form-control"/>
+                <label for="intro">Inleiding</label><input name="intro" id="intro" value="<?php echo $values['intro'];?>" type="text" class="form-control"/>
+                <label for="content">Bericht</label><textarea name="content" value="<?php echo $values['content'];?>" id="content" class="form-control"></textarea>
+                <label for="author">Maker</label><input name="author" value="<?php echo $values['author'];?>" id="author" class="form-control" >
+                <label for="rev_date">Datum</label><br/><input name="rev_date" value="<?php echo $values['rev_date'];?>" id="rev_date" class="form-control"><br/>
+                <label for="image">Afbeelding</label><input name="image" value="<?php echo $values['image'];?>" id="image" class="form-control" >
                 <br />
                 <input type="submit" class="btn btn-default" name="addReview" value="Verstuur"/>
             </form>
