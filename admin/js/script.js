b6ec4580a63b021651a0397a40879e91fd8fab39
Alwin <alwinkroesen@gmail.com>
2015-05-21 11:08:16 +0200

    een boel

--- admin/js/script.js
+++ admin/js/script.js
@@ -11,7 +11,6 @@ $(document).ready(function(){
 
 var rev_start = 0;
 var rev_end = 10;
-var rev_max = 0;
 function reviews(){
 
     var content = document.querySelector("#reviewContents");
@@ -24,52 +23,56 @@ function reviews(){
         data: {start: rev_start,end: rev_end},
         dataType: 'json'
     }).done(function(data){
-        var table = document.createElement("table");
-        table.setAttribute("class", "table");
-        table.style.width = "100%";
 
-        for(var i =0;i < data.length;i++){
-            //createDivs(searchContent, "<a href='?readMore="+data[i].id+"'>"+data[i].title+"</a> <br />");
-            var tr = document.createElement("tr");
-            var td = document.createElement("td");
-            td.innerHTML = data[i].title;
-            tr.appendChild(td);
-            td = document.createElement("td");
-            td.innerHTML = data[i].rev_date;
-            tr.appendChild(td);
-            table.appendChild(tr);
-        }
-        content.appendChild(table);
-
-        //console.log(data);
-    });
-
-
-    $.ajax({
-        url: "admin/includes/Ajax.php?reviewsMax"
-    }).done(function(data){
-        rev_max = data;
-        if(rev_max > rev_end){
-
+        $.ajax({
+            url: "admin/includes/Ajax.php?reviewsMax"
+        }).done(function(rev_max){
             // button previous
-            var bttnPrev = document.createElement("input");
-            bttnPrev.type = "button";
-            bttnPrev.value = "Vorige";
-            content.appendChild(bttnPrev);
+            if(rev_start != 0){
+                var bttnPrev = document.createElement("input");
+                bttnPrev.type = "button";
+                bttnPrev.value = "Vorige";
+                content.appendChild(bttnPrev);
 
-            bttnPrev.onclick = function(){
-                console.log("prev");
-            };
+                bttnPrev.onclick = function(){
+                    rev_end -= 10;
+                    rev_start -= 10;
+                    reviews();
+                };
+            }
 
             // button next
-            var bttnNext = document.createElement("input");
-            bttnNext.type = "button";
-            bttnNext.value = "Volgende";
-            content.appendChild(bttnNext);
+            if(rev_max > rev_end){
+                var bttnNext = document.createElement("input");
+                bttnNext.type = "button";
+                bttnNext.value = "Volgende";
+                content.appendChild(bttnNext);
 
-            bttnNext.onclick = function(){
-                console.log("Next");
-            };
-        }
+                bttnNext.onclick = function(){
+                    rev_end += 10;
+                    rev_start += 10;
+                    reviews();
+                };
+            }
+
+            var table = document.createElement("table");
+            table.setAttribute("class", "table");
+            table.style.width = "100%";
+
+            for(var i =0;i < data.length;i++){
+                //createDivs(searchContent, "<a href='?readMore="+data[i].id+"'>"+data[i].title+"</a> <br />");
+                var tr = document.createElement("tr");
+                var td = document.createElement("td");
+                td.innerHTML = data[i].title;
+                tr.appendChild(td);
+                td = document.createElement("td");
+                td.innerHTML = data[i].rev_date;
+                tr.appendChild(td);
+                table.appendChild(tr);
+            }
+            content.appendChild(table);
+        });
+
     });
+
 }
